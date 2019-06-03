f9533c1a36114022c78e85de9b8d333785afba12
ZHU Chenyu <chenyu_zhu@protonmail.com>
2019-06-03 18:50:15 +0800

    Some bugs have been modified

--- twitter_spider_noCommment.py
+++ twitter_spider_noCommment.py
@@ -1,24 +1,10 @@
 import time
 from bs4 import BeautifulSoup
-from pymongo import MongoClient
 from selenium import webdriver
 import datetime
-import pymongo
 import json
 
-fp = open('data_twitter_noComments.json','a')
 
-
-'''set mongodb Client'''
-'''
-Client = MongoClient('mongodb://localhost:27017/')
-db = Client['twitterData']
-collection = db.twitter
-'''
-
-'''setting login account'''
-usrname = 'chenyu_zhu@protonmail.com'
-password = 'IrishWhiskey233'
 
 def yearTodDays(year):
     dayCount = 0
@@ -51,7 +37,7 @@ def searchTimeSplit(time_start, time_end):
     date_startTime = datetime.date(startTimeArr[0],startTimeArr[1],startTimeArr[2])
     timeSpan = [str(date_startTime)]
     date_afterPlus = date_startTime
-    for _ in range(dayCount+1):
+    for _ in range(dayCount):
         delta = datetime.timedelta(days=1)
         date_afterPlus = date_afterPlus + delta
         timeSpan.append(str(date_afterPlus))
@@ -109,11 +95,12 @@ getTweetUrl = lambda tweet_id: 'https://twitter.com/APompliano/status/{}'.format
 
 def scrollPageToEnd(browser):
     time_scrollStart = time.time()
-    while(time.time() - time_scrollStart < 180):
+    while(time.time() - time_scrollStart < 600):
         try:
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            browser.implicitly_wait(0.5)
         except:
-           time.sleep(2)
+           time.sleep(1)
         
     print('scroll finished, start collecting')
 
@@ -132,8 +119,8 @@ def collectTweetData(browser,keywords, time_start, time_end):
     bs = BeautifulSoup(browser.page_source,"html.parser")
     tweets_all = bs.select('.tweet')
     for tweet in tweets_all:
-        if (tweet.get('data-user-id') != None):
-            tweet_nickname = tweet.select_one('.fullname').text
+        if (tweet.get('data-user-id') is not None and tweet.select_one('.tweet-text') is not None):
+            tweet_nickname = tweet.get('data-name')
             tweet_usrId = tweet.get('data-user-id')
             tweet_content = tweet.select_one('.tweet-text').text
             tweet_tweetId = tweet.get('data-tweet-id')
@@ -181,137 +168,16 @@ def collectTweetData(browser,keywords, time_start, time_end):
             except:
                 tweet_favoriteNum = int(float(tweet_favoriteNum.split('K')[0])*1000)
             
-            tweet_loneReply = ['None']
-            tweet_convReply = ['None']
-            '''
-            if (tweet_replyNum != 0):
-                tweet_url =  getTweetUrl(tweet_tweetId)
-                browserForGetReply.get(tweet_url)
-                scrollPageToEnd()
-                bs_loneReply = BeautifulSoup(browserForGetReply.page_source, "html.parser")
-                if (bs_loneReply.select_one('.ThreadedConversation--loneTweet') != None):
-                    print('the tweet ' + str(tweet_tweetId) + ' has lone reply, collecting now')
-                    tweet_loneReply.remove('None')
-                    loneReply_all = bs_loneReply.select('.ThreadedConversation--loneTweet')
-                    for loneReply_oneReply in loneReply_all:
-                        if loneReply_oneReply.select_one('.tweet') != None:
-                            tweet_loneReply_oneReply = loneReply_oneReply.select_one('.tweet')
-                            loneReply_tweetId = tweet_loneReply_oneReply.get('data-item-id')
-                            loneReply_usrId = tweet_loneReply_oneReply.select_one('.fullname').text
-                            loneReply_tweetTime = tweet_loneReply_oneReply.select_one('.tweet-timestamp').get('data-original-title')
-                            if loneReply_tweetTime == None:
-                                loneReply_tweetTime = tweet_loneReply_oneReply.select_one('.tweet-timestamp').get('title')
-                            loneReply_tweetTime_afterProcess = tweetTime_process(loneReply_tweetTime)
-                            loneReply_content = tweet_loneReply_oneReply.select_one('.tweet-text').text
-                            loneReply_replyNum = 0
-
-                            loneReply_actions_all = tweet_loneReply_oneReply.select_one('.ProfileTweet-actionList')
-
-                            loneReply_action_retweet = loneReply_actions_all.select_one('.ProfileTweet-action--retweet')
-                            if loneReply_action_retweet.select_one('.ProfileTweet-actionCount--isZero') != None:
-                                loneReply_retweetNum = 0 
-                            elif loneReply_action_retweet.select_one('.ProfileTweet-actionCountForPresentation') != None:
-                                loneReply_retweetNum = loneReply_action_retweet.select_one('.ProfileTweet-actionCountForPresentation').text
-                            else:
-                                loneReply_retweetNum = loneReply_action_retweet.select_one('.ProfileTweet-actionCount').get('data-tweet-stat-count')
-
-                            loneReply_action_favorite = loneReply_actions_all.select_one('.ProfileTweet-action--favorite')
-                            if loneReply_action_favorite.select_one('.ProfileTweet-actionCount--isZero') != None:
-                                loneReply_favoriteNum = 0 
-                            elif loneReply_action_favorite.select_one('.ProfileTweet-actionCountForPresentation') != None:
-                                loneReply_favoriteNum = loneReply_action_favorite.select_one('.ProfileTweet-actionCountForPresentation').text
-                            else:
-                                loneReply_favoriteNum = loneReply_action_favorite.select_one('.ProfileTweet-actionCount').get('data-tweet-stat-count')
-                            loneReply_retweetNum = int(loneReply_retweetNum)
-                            loneReply_favoriteNum = int(loneReply_favoriteNum)
-                            loneReply_oneTweet = {'loneReply_tweetId': loneReply_tweetId,
-                                                  'loneReply_usrId': loneReply_usrId,
-                                                  'loneReply_tweetTime': loneReply_tweetTime_afterProcess,
-                                                  'loneReply_content': loneReply_content,
-                                                  'loneReply_replyNum': loneReply_replyNum,
-                                                  'loneReply_retweetNum': loneReply_retweetNum,
-                                                  'loneReply_favoriteNum': loneReply_favoriteNum
-                                                  }
-                            tweet_loneReply.append(loneReply_oneTweet)
-                    print('the lone reply in tweet ' + str(tweet_tweetId) + ' has been collected')
-                elif (bs_loneReply.select_one('.ThreadedConversation') != None):
-                    print('the tweet' + str(tweet_tweetId) + ' has conversation reply, collecting now')
-                    tweet_convReply.remove('None')
-                    try:
-                        convReply_moreItems_all = browserForGetReply.find_elements_by_class_name('ThreadedConversation-moreReplies')
-                        for covReply_moreItem in convReply_moreItems_all:
-                            moreReplyButton = covReply_moreItem.find_element_by_class_name('ThreadedConversation-moreRepliesLink')
-                            moreReplyButton.click()
-                            time.sleep(0.5)
-                    except:
-                        pass
-                    bs_convReply = BeautifulSoup(browserForGetReply.page_source, "html.parser")
-                    convReply_all = bs_convReply.select('.ThreadedConversation')
-                    
-                    for convReply_oneReply in convReply_all:
-                        convReply_oneTweet = []
-                        tweet_convReply_oneTweet = convReply_oneReply.select('.tweet')
-                        for tweet_convReply_oneReply in tweet_convReply_oneTweet:
-                            if tweet_convReply_oneReply != None:
-                                convReply_tweetId = tweet_convReply_oneReply.get('data-item-id')
-                                convReply_usrId = tweet_convReply_oneReply.select_one('.fullname').text
-                                convReply_tweetTime = tweet_convReply_oneReply.select_one('.tweet-timestamp').get('data-original-title')
-                                if convReply_tweetTime == None:
-                                    convReply_tweetTime = tweet_convReply_oneReply.select_one('.tweet-timestamp').get('title')
-                                convReply_tweetTime_afterProcess = tweetTime_process(convReply_tweetTime)
-                                convReply_content = tweet_convReply_oneReply.select_one('.tweet-text').text
-
-                                convReply_actions_all = tweet_convReply_oneReply.select_one('.ProfileTweet-actionList')
-
-                                convReply_action_reply = convReply_actions_all.select_one('.ProfileTweet-action--reply')
-                                if convReply_action_reply.select_one('.ProfileTweet-actionCount--isZero') != None:
-                                    convReply_replyNum = 0 
-                                elif convReply_action_reply.select_one('.ProfileTweet-actionCountForPresentation') != None:
-                                    convReply_replyNum = convReply_action_reply.select_one('.ProfileTweet-actionCountForPresentation').text
-                                else:
-                                    convReply_replyNum = convReply_action_reply.select_one('.ProfileTweet-actionCount').get('data-tweet-stat-count')
-
-                                convReply_action_retweet = convReply_actions_all.select_one('.ProfileTweet-action--retweet')
-                                if convReply_action_retweet.select_one('.ProfileTweet-actionCount--isZero') != None:
-                                    convReply_retweetNum = 0 
-                                elif convReply_action_retweet.select_one('.ProfileTweet-actionCountForPresentation') != None:
-                                    convReply_retweetNum = convReply_action_retweet.select_one('.ProfileTweet-actionCountForPresentation').text
-                                else:
-                                    convReply_retweetNum = convReply_action_retweet.select_one('.ProfileTweet-actionCount').get('data-tweet-stat-count')
-
-                                convReply_action_favorite = convReply_actions_all.select_one('.ProfileTweet-action--favorite')
-                                if convReply_action_favorite.select_one('.ProfileTweet-actionCount--isZero') != None:
-                                    convReply_favoriteNum = 0 
-                                elif convReply_action_favorite.select_one('.ProfileTweet-actionCountForPresentation') != None:
-                                    convReply_favoriteNum = convReply_action_favorite.select_one('.ProfileTweet-actionCountForPresentation').text
-                                else:
-                                    convReply_favoriteNum = convReply_action_favorite.select_one('.ProfileTweet-actionCount').get('data-tweet-stat-count')
-
-                                convReply_replyNum = int(convReply_replyNum)
-                                convReply_retweetNum = int(convReply_retweetNum)
-                                convReply_favoriteNum = int(convReply_favoriteNum)
-                                convReply_oneReply = {'convReply_usrId': convReply_usrId,
-                                                      'convReply_content': convReply_content,
-                                                      'convReply_tweetId': convReply_tweetId,
-                                                      'convReply_tweetTime': convReply_tweetTime_afterProcess,
-                                                      'convReply_replyNum': convReply_replyNum,
-                                                      'convReply_retweetNum': convReply_retweetNum,
-                                                      'convReply_favoriteNum': convReply_favoriteNum
-                                                      }
-                                convReply_oneTweet.append(convReply_oneReply)
-                        tweet_convReply.append(convReply_oneTweet)
-            '''            
-            tweetData_collected_oneTweet = {'tweet_nickname': tweet_nickname,
-                                            'tweet_usrId': tweet_usrId,
-                                            'tweet_content': tweet_content,
-                                            'tweet_tweetId': tweet_tweetId,
-                                            'tweet_tweetTime': tweet_tweetTime_afterProcess,
-                                            'tweet_replyNum': tweet_replyNum,
-                                            'tweet_favoriteNum': tweet_favoriteNum,
-                                            'tweet_retweetNum': tweet_retweetNum,
-                                            'tweet_loneReply': tweet_loneReply,
-                                            'tweet_convReply': tweet_convReply
-                                            }
+            
+            tweetData_collected_oneTweet = [tweet_nickname,
+                                            tweet_usrId,
+                                            tweet_content,
+                                            tweet_tweetId,
+                                            tweet_tweetTime_afterProcess,
+                                            tweet_replyNum,
+                                            tweet_favoriteNum,
+                                            tweet_retweetNum,
+                                        ]
             
             #print(tweetData_collected_oneTweet)
             tweetData_collected.append(tweetData_collected_oneTweet)
@@ -323,10 +189,15 @@ def collectTweetData(browser,keywords, time_start, time_end):
 
                         
 if __name__ == '__main__':
+
     '''covered search area'''
     keywords = ['bitcoin', 'BTC']
     time_start = '2019-01-01'
-    time_end = '2019-05-29'
+    time_end = '2019-05-19'
+
+    fileName = 'data_twitter_noComments_from{}_to{}'.format(time_start,time_end)
+
+    fp = open(fileName,'w')
 
 
     '''Log in twitter'''
@@ -337,48 +208,19 @@ if __name__ == '__main__':
     #browserForGetReply = webdriver.Chrome()
     browser.set_page_load_timeout(30)
     #browserForGetReply.set_page_load_timeout(30)
-    '''
-    url = 'https://twitter.com/login?lang=en'
 
-    print("tring to log in twitter right now")
-    browser.get(url)
-    my_usrname = browser.find_element_by_class_name('js-username-field')
-    #my_usrname.click()
-    my_usrname.send_keys(usrname)
-    my_password = browser.find_element_by_class_name('js-password-field')
-    my_password.click()
-    my_password.send_keys(password)
-    time.sleep(3)
-    my_login = browser.find_element_by_class_name('EdgeButtom--medium')
-    my_login.click()
-    print('login accomplished')
-    '''
-    '''
-    browserForGetReply.get(url)
-    my_usrname1 = browserForGetReply.find_element_by_class_name(
-        'js-username-field')
-    my_usrname1.click()
-    my_usrname1.send_keys(usrname)
-    time.sleep(2)
-    my_password2 = browserForGetReply.find_element_by_class_name(
-        'js-password-field')
-    my_password2.click()
-    my_password2.send_keys(password)
-    time.sleep(2)
-    my_login2 = browserForGetReply.find_element_by_class_name('EdgeButtom--medium')
-    my_login2.click()
-    
-    '''
-
-    tweetData_all = []
+    tweetData_all = [[],[],[],[],[],[],[],[],[],[]]
     tweetData_count_all = 0
     timeSearchSpan = searchTimeSplit(time_start,time_end)
     for i in range(len(timeSearchSpan) - 1):
         tweetData_oneDay, tweetData_count_oneDay = collectTweetData(browser,keywords, timeSearchSpan[i], timeSearchSpan[i+1])
-        tweetData_all.append(tweetData_oneDay)
+        for j in range(len(tweetData_oneDay)):
+            for k in range(len(tweetData_all)):
+                tweetData_all[k].append(tweetData_oneDay[j][k])
         tweetData_count_all += tweetData_count_oneDay
     #collection.insert_one(tweetData_all)
     json.dump(tweetData_all,fp,ensure_ascii=False)
     print('----all tweets data has been collected successfully!----')
     print('total: ', tweetData_count_all)
+    fp.close()
 
